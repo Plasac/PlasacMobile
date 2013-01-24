45aa70208889998028f9a45a5e5273997dbfaac3
Paulo Sergio <paulo_bsi@yahoo.com.br>
2013-01-24 16:15:25 -0200

    Inicio interface

--- src/br/com/plasac/MainActivity.java
+++ src/br/com/plasac/MainActivity.java
@@ -1,22 +1,32 @@
 package br.com.plasac;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private static final String[] credenciados = { "","HSC", "Plasac" };
+	ArrayAdapter<String> mCredenciados;
+	Spinner spnCredenciados;
+	 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		 
+		mCredenciados = new ArrayAdapter<String>(this,
+		android.R.layout.simple_spinner_item, credenciados);
+		spnCredenciados = (Spinner) findViewById(R.id.spinner1);
+		spnCredenciados.setAdapter(mCredenciados);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
 	}
-
 }
